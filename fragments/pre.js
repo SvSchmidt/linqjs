5fa3ad4103d461ce005c1bf67bfeac59e54c3947
Gerriet Hinrichs <hinrichs@hanseaticsoft.com>
2017-12-04 16:48:11 +0100

    remove old js sources and fragments

--- fragments/pre.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict';
-
-  // this || (0, eval)('this') is a robust way for getting a reference
-  // to the global object
-  const window = this || (0, eval)('this'); // jshint ignore:line
-  const DEBUG = DEBUG_CONSTANT_VALUE;
